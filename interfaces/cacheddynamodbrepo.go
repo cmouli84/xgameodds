feaf1e18fe0155f3196c7c0d6629ed54dcc553e9
Chandramouli S <cmouli_84@yahoo.co.in>
2016-11-27 22:42:43 -0800

    Fixed caching issue in dynamo db repo

--- interfaces/cacheddynamodbrepo.go
+++ interfaces/cacheddynamodbrepo.go
@@ -1,6 +1,7 @@
 package interfaces
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/cmouli84/xgameodds/domain"
@@ -37,7 +38,7 @@ func (cachedPersistedRankingRepo *CachedPersistedRankingRepo) GetNflPersistedRan
 
 func (cachedPersistedRankingRepo *CachedPersistedRankingRepo) getPersistedRanking(eventIds []int, getPersistedRankingFn getPersistedRanking, dataCache *cache.Cache) map[int]domain.PersistedRanking {
 	var persistedRankingInterface interface{}
-	var persistedRankingPtr *domain.PersistedRanking
+	var persistedRankingPtr domain.PersistedRanking
 	var found bool
 	persistedRankingMap := make(map[int]domain.PersistedRanking)
 	notFoundEventIds := make([]int, 0)
@@ -46,21 +47,28 @@ func (cachedPersistedRankingRepo *CachedPersistedRankingRepo) getPersistedRankin
 		persistedRankingInterface, found = dataCache.Get(strconv.Itoa(eventID))
 
 		if found {
-			persistedRankingPtr = persistedRankingInterface.(*domain.PersistedRanking)
-			persistedRankingMap[eventID] = *persistedRankingPtr
+			persistedRankingPtr = persistedRankingInterface.(domain.PersistedRanking)
+			persistedRankingMap[eventID] = persistedRankingPtr
 		} else {
 			notFoundEventIds = append(notFoundEventIds, eventID)
 		}
 	}
+	fmt.Println("From Cache", persistedRankingMap)
 
 	if len(notFoundEventIds) > 0 {
 		persistedRankingRepo := getPersistedRankingFn(notFoundEventIds)
+		fmt.Println("From Repo", persistedRankingRepo)
 
 		for k, v := range persistedRankingRepo {
 			persistedRankingMap[k] = v
-			dataCache.Set(strconv.Itoa(k), &v, cache.DefaultExpiration)
+			dataCache.Set(strconv.Itoa(k), persistedRankingRepo[k], cache.DefaultExpiration)
+
+			persistedRankingInterface, found = dataCache.Get(strconv.Itoa(k))
+			fmt.Println(k, persistedRankingInterface.(domain.PersistedRanking).HomeRanking, persistedRankingInterface.(domain.PersistedRanking).AwayRanking)
 		}
 	}
+
+	fmt.Println("Consolidated", persistedRankingMap)
 
 	return persistedRankingMap
 }
